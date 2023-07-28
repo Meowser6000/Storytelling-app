c935ea03ecb2330d9f97fd5b0df97ac818385666
Earthling <90159624+Meowser6000@users.noreply.github.com>
2023-07-27 20:43:45 -0400

    Add files via upload

---
+++ ST-88-Solution-master/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRwf0bPS_NJNYJU7dtWX7-pVGUKEj0-sE",
+  authDomain: "storytelling-app-160a9.firebaseapp.com",
+  databaseURL: "https://storytelling-app-160a9-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-160a9",
+  storageBucket: "storytelling-app-160a9.appspot.com",
+  messagingSenderId: "9202455471",
+  appId: "1:9202455471:web:7598061d027ca4ac5d555a"
+};
